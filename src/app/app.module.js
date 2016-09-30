bff241fcbc0adc6bb288224fd8e72f74e855e659
Thiago R. M. Bitencourt <thiago.bitencourt@senior.com.br>
2016-09-30 13:52:14 -0300

    Load app on index

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,8 +1,8 @@
 /** Dependencies modules imports */
-import mainPanel from './modules/mainPanel/mainPanel.module';
+import './modules/mainPanel/mainPanel.module';
 
 export default (() => {
   return angular.module('app', [
-    mainPanel
+    'mainPanel'
   ]);
 })();
