deaf23a557a787c5b4afe8164d1bae948b079425
Thiago R. M. Bitencourt <thiago.bitencourt@senior.com.br>
2016-09-30 16:47:13 -0300

    Ajust some gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,25 @@
 'use strict';
+var gulp = require('gulp');
+var bowerFiles = require('main-bower-files');
 
-const gulp = require('gulp');
-const bowerFiles = require('main-bower-files');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var babelify = require('babelify');
+var browserify = require('browserify');
 
-const source = require('vinyl-source-stream');
-const buffer = require('vinyl-buffer');
-const babelify = require('babelify');
-const browserify = require('browserify');
-
-const size = require('gulp-size');
-const uglify = require('gulp-uglify');
-const gulpif = require('gulp-if');
+var size = require('gulp-size');
+var uglify = require('gulp-uglify');
+var gulpif = require('gulp-if');
 var replace = require('gulp-replace');
+var clean = require('gulp-clean');
 
-// const argv = require('yargs').argv; /* Not used yet */
-const Server = require('karma').Server;
+// var argv = require('yargs').argv; /* Not used yet */
+var Server = require('karma').Server;
 
 var fs = require('fs');
 var json = JSON.parse(fs.readFileSync('./package.json'));
 
-const config = {
+var config = {
   src: {
     base: './src/',
     entry: 'src/app/app.module.js',
@@ -34,8 +34,15 @@ const config = {
   libs: {
     src: './src/assets/libs',
     dest: './dist/assets/libs'
-  }
+  },
+  replace: '@@VERSION@@'
 }
+
+/**
+ * Transpile code 6to5
+ */
+gulp.task('transpile:dist', () => { transpile() });
+gulp.task('transpile:dev', () => { transpile(true) });
 
 function transpile(development) {
   if(development) {
@@ -44,7 +51,7 @@ function transpile(development) {
       .bundle()
       .pipe(source(config.src.dest))
       .pipe(buffer())
-      .pipe(replace('@@VERSION@@', json.version))
+      .pipe(replace(config.replace, json.version)) // Replace @@VERSION@@
       // .pipe(gulpif(development, uglify())) //gulp-if not working
       .pipe(size())
       .pipe(gulp.dest(config.folder.dest.app));
@@ -54,7 +61,7 @@ function transpile(development) {
       .bundle()
       .pipe(source(config.src.dest))
       .pipe(buffer())
-      .pipe(replace('@@VERSION@@', json.version))
+      .pipe(replace(config.replace, json.version))
       .pipe(uglify())
       .pipe(size())
       .pipe(gulp.dest(config.folder.dest.app));
@@ -62,7 +69,9 @@ function transpile(development) {
 }
 
 gulp.task('copyhtml', () => {
-  return gulp.src([config.src.base + '**/*.html']).pipe(gulp.dest('./dist/'));
+  return gulp.src([config.src.base + '**/*.html'])
+    .pipe(replace(config.replace, json.version))
+    .pipe(gulp.dest('./dist/'));
 });
 
 /**
@@ -73,33 +82,29 @@ gulp.task('copybower', () => {
   return gulp.src( bowerFiles(), {base: config.libs.src} ).pipe( gulp.dest(config.libs.dest) );
 });
 
-
-/**
- * Inject the version number into the files
- */
-
-
-gulp.task('versioningHtml', () => {
-  return gulp.src([config.src.base + '**/*.html'])
-    .pipe(replace('@@VERSION@@', json.version))
-    .pipe(gulp.dest(config.folder.dest.base));
-});
-
-gulp.task('transpile:dist', () => { transpile() });
-gulp.task('transpile:dev', () => { transpile(true) });
-
 /**
  * Run test once and exit
  */
-gulp.task('test', function (done) {
+gulp.task('test', (done) => {
   new Server({
     configFile: __dirname + '/karma.conf.js',
     singleRun: true
   }, done() ).start();
 });
 
-gulp.task('common', ['copyhtml', 'copybower', 'versioningHtml']);
+/** Clear any old file from dits directory */
+gulp.task('clean', () => {
+  gulp.src('dist/', {read: false})
+    .pipe(clean());
+});
+
+gulp.task('common', ['copyhtml', 'copybower']);
+/** DO NOT use this directly */
+gulp.task('dev-ahead', ['transpile:dev', 'common']);
+gulp.task('dist-ahead', ['transpile:dist', 'common']);
 
 /** Default and most used tasks */
-gulp.task('dev', ['transpile:dev', 'common']);
-gulp.task('default', ['transpile:dist', 'common']);
+gulp.task('dev', ['clean'], () => { setTimeout(() => { gulp.start('dev-ahead') }, 100) });
+gulp.task('dist', ['clean'], () => { setTimeout(() => { gulp.start('dist-ahead') }, 100) });
+
+gulp.task('default', ['dist']);
