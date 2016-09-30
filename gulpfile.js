eb85b05ded56b963125c314a865b4ec00a2fd87a
Thiago R. M. Bitencourt <thiago.bitencourt@senior.com.br>
2016-09-30 15:56:24 -0300

    Add some more gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const gulp = require('gulp');
 const bowerFiles = require('main-bower-files');
 
@@ -5,13 +7,62 @@ const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
 const babelify = require('babelify');
 const browserify = require('browserify');
+
 const size = require('gulp-size');
-
 const uglify = require('gulp-uglify');
+const gulpif = require('gulp-if');
+var replace = require('gulp-replace');
 
-gulp.task('copy', () => {
-  return gulp.src(['./src/index.html',])
-  .pipe(gulp.dest('./dist/'));
+// const argv = require('yargs').argv; /* Not used yet */
+const Server = require('karma').Server;
+
+var fs = require('fs');
+var json = JSON.parse(fs.readFileSync('./package.json'));
+
+const config = {
+  src: {
+    base: './src/',
+    entry: 'src/app/app.module.js',
+    dest: 'app.module.js'
+  },
+  folder: {
+    dest: {
+      base: 'dist/',
+      app:'dist/app/' 
+    }
+  },
+  libs: {
+    src: './src/assets/libs',
+    dest: './dist/assets/libs'
+  }
+}
+
+function transpile(development) {
+  if(development) {
+    browserify({ entries: config.src.entry, debug: true })
+      .transform(babelify)
+      .bundle()
+      .pipe(source(config.src.dest))
+      .pipe(buffer())
+      .pipe(replace('@@VERSION@@', json.version))
+      // .pipe(gulpif(development, uglify())) //gulp-if not working
+      .pipe(size())
+      .pipe(gulp.dest(config.folder.dest.app));
+  } else {
+    browserify({ entries: config.src.entry, debug: true })
+      .transform(babelify)
+      .bundle()
+      .pipe(source(config.src.dest))
+      .pipe(buffer())
+      .pipe(replace('@@VERSION@@', json.version))
+      .pipe(uglify())
+      .pipe(size())
+      .pipe(gulp.dest(config.folder.dest.app));
+  }
+}
+
+gulp.task('copyhtml', () => {
+  return gulp.src([config.src.base + '**/*.html']).pipe(gulp.dest('./dist/'));
 });
 
 /**
@@ -19,19 +70,36 @@ gulp.task('copy', () => {
  * It evaluates the bower.json config filte to get the dependencies to be copied.
  */
 gulp.task('copybower', () => {
-  return gulp.src(bowerFiles(), {base: './src/assets/libs'})
-  .pipe(gulp.dest('./dist/assets/libs'));
+  return gulp.src( bowerFiles(), {base: config.libs.src} ).pipe( gulp.dest(config.libs.dest) );
 });
 
-gulp.task('transpile', () => {
-  browserify({ entries: 'src/app/app.module.js', debug: true })
-  .transform(babelify)
-  .bundle()
-  .pipe(source('app.module.js'))
-  .pipe(buffer())
-  .pipe(uglify())
-  .pipe(size())
-  .pipe(gulp.dest('dist/app/'));
+
+/**
+ * Inject the version number into the files
+ */
+
+
+gulp.task('versioningHtml', () => {
+  return gulp.src([config.src.base + '**/*.html'])
+    .pipe(replace('@@VERSION@@', json.version))
+    .pipe(gulp.dest(config.folder.dest.base));
 });
 
-gulp.task('default', ['transpile', 'copy', 'copybower']);
+gulp.task('transpile:dist', () => { transpile() });
+gulp.task('transpile:dev', () => { transpile(true) });
+
+/**
+ * Run test once and exit
+ */
+gulp.task('test', function (done) {
+  new Server({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done() ).start();
+});
+
+gulp.task('common', ['copyhtml', 'copybower', 'versioningHtml']);
+
+/** Default and most used tasks */
+gulp.task('dev', ['transpile:dev', 'common']);
+gulp.task('default', ['transpile:dist', 'common']);
--- src/app/app.module.js
+++ src/app/app.module.js
@@ -4,5 +4,10 @@ import './modules/mainPanel/mainPanel.module';
 export default (() => {
   return angular.module('app', [
     'mainPanel'
-  ]);
+  ])
+  .config(() => {
+    let version = "@@VERSION@@";
+    localStorage.setItem('es6-seed', version);
+    window.version = version;
+  });
 })();
