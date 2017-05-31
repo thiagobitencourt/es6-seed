8b41cbf74e154309678255d2b21315813936269a
Thiago Bitencourt <thiago.bitencourt@senior.com.br>
2017-05-31 08:02:56 -0300

    Change build from gulp to webpack
    #3

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // Generate the index.html
+const packageJson = require('./package');
+
+let config = {
+  entry: './src/app/app.module.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'app.module.js'
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'ES6-SEED',
+      version: packageJson.version,
+      name: packageJson.name,
+      template: './src/index.html'
+    })
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist'), // A directory or URL to server HTML content from.
+    historyApiFallback: true, // fallback to /index.html for Single Page Applications.
+    inline: true, // inline mode (set to false to disable including client scripts (like livereload))
+    open: true // open default browser while lauching
+  },
+  devtool: 'source-map'
+}
+
+module.exports = config;
+
+if(process.env.NODE_ENV && process.env.NODE_ENV.trim() === 'production') {
+  module.exports.plugins.push(
+    new webpack.optimize.UglifyJsPlugin()
+  )
+}
